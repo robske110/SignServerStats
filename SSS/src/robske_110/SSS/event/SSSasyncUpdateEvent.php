8da13a6e639bd7f605792a547b374db390d6ed5d
Tim (robske_110) <robske110@users.noreply.github.com>
2017-10-19 20:49:01 +0200

    SSS: Refine Event
    still part of API 1.1.0

--- SSS/src/robske_110/SSS/event/SSSasyncUpdateEvent.php
+++ SSS/src/robske_110/SSS/event/SSSasyncUpdateEvent.php
@@ -9,13 +9,20 @@ class SSSasyncUpdateEvent extends PluginEvent{
 	private $lastUpdate;
 	/** @var int */
 	private $currUpdate;
+	/** @var SignServerStats */
+	private $sss;
 	
 	public static $handlerList = null;
 	
 	public function __construct(SignServerStats $sss, int $lastUpdate, int $currUpdate){
 		parent::__construct($sss);
+		$this->sss = $sss;
 		$this->lastUpdate = $lastUpdate;
 		$this->currUpdate = $currUpdate;
+	}
+	
+	public function getSSS(): SignServerStats{
+		return $this->sss;
 	}
 	
 	public function getCurrUpdate(): int{
