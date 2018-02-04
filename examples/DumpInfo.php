740fcc576308c19365ce82de6b746007ba26786b
Tim (robske_110) <robske110@users.noreply.github.com>
2018-02-04 20:29:52 +0100

    Bump everything to ALPHA11

--- examples/DumpInfo.php
+++ examples/DumpInfo.php
@@ -4,7 +4,7 @@
  * @name DumpServerInfo
  * @main robske_110\DPS\DumpServerInfo
  * @version 1.0.0
- * @api 3.0.0-ALPHA10
+ * @api 3.0.0-ALPHA11
  * @description Dumps query info of a Server using SignServerStats
  * @author robske_110
  * @license MIT
